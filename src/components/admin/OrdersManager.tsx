7e6eb05c51300e9a9db3a79b67c69a2f2e20b802
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2026-05-28 14:26:32 +0300

    Update OrdersManager.tsx

--- src/components/admin/OrdersManager.tsx
+++ src/components/admin/OrdersManager.tsx
@@ -240,7 +240,7 @@ export default function OrdersManager() {
           )}
         </div>
 
-        <div className="grid grid-cols-3 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
           {/* Date picker */}
           <div className="relative">
             <button type="button" onClick={() => { setShowDatePicker(!showDatePicker); setShowServiceFilter(false); setShowStatusFilter(false); }}
